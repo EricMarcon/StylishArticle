b66f68f93b9fe3313cea04692b7cdad4d19f9902
Eric Marcon <eric.marcon@agroparistech.fr>
2023-03-06 09:24:26 +0100

    OK except for tables

--- _extensions/stylisharticle/color-text.lua
+++ _extensions/stylisharticle/color-text.lua
@@ -1,16 +1,10 @@
---[[
-  If you extension format needs some special processing, include a Lua filter to be used in addition of Quarto built-in ones.
-  Quarto exports utils function that can be used in all filters. See 
-  https://github.com/quarto-dev/quarto-cli/blob/main/src/resources/pandoc/datadir/init.lua#L1522-L1576
-]]--
+-- Allow [content]{color=<name>}
 
--- Example: allow to color words by using a span attribute.
--- This filter will use the correct syntax depending on the format
 color_span = function(el)
   color = el.attributes['color']
   -- if no color attribute, return unchange
   if color == nil then return el end
-  
+
   -- transform to <span class="color-*"></span>
   if quarto.doc.isFormat("html") then
     -- remove color attributes
